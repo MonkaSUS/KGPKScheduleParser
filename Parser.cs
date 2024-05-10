986683355c041ee95ffebab9ea8187827820b229
Lev <Lev@DESKTOP-4LIIN5Q>
2024-05-10 20:01:49 +0300

    Parser wokrs fine, but somee host does not have enough disk space to run chromedriver, thus craching the app on incoming request

--- Parser.cs
+++ Parser.cs
@@ -1,22 +1,58 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using KGPKScheduleParser;
+using Microsoft.AspNetCore.Components.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
 using System.Data;
 
 namespace KGPKScheduleParser
 {
+    /// <summary>
+    /// ОСТАВЬ НАДЕЖДУ ВСЯК СЮДА ВХОДЯЩИЙ
+    ///    ⠀⠀⠀⢀⣀⣤⣤⠴⠶⠶⠶⠶⠶⠶⠶⠶⢤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⢀⣤⠶⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠶⣤⡀⠀⠀⠀⠀⠀
+    ///⠀⠀⢀⡴⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢷⡄⠀⠀⠀
+    ///⠀⣰⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣦⠀⠀
+    ///⢰⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⠀
+    ///⣿⠀⠀⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡄⠀⢹⡄
+    ///⡏⠀⢰⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⢸⡇
+    ///⣿⠀⠘⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀⢸⡇
+    ///⢹⡆⠀⢹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠃⠀⣾⠀
+    ///⠈⢷⡀⢸⡇⠀⢀⣠⣤⣶⣶⣶⣤⡀⠀⠀⠀⠀⠀⢀⣠⣶⣶⣶⣶⣤⣄⠀⠀⣿⠀⣼⠃⠀
+    ///⠀⠈⢷⣼⠃⠀⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⡇⠀⢸⡾⠃⠀⠀
+    ///⠀⠀⠈⣿⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⠃⠀⢸⡇⠀⠀⠀
+    ///⠀⠀⠀⣿⠀⠀⠘⢿⣿⣿⣿⣿⡿⠃⠀⢠⠀⣄⠀⠀⠙⢿⣿⣿⣿⡿⠏⠀⠀⢘⡇⠀⠀⠀
+    ///⠀⠀⠀⢻⡄⠀⠀⠀⠈⠉⠉⠀⠀⠀⣴⣿⠀⣿⣷⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⢸⡇⠀⠀⠀
+    ///⠀⠀⠀⠈⠻⣄⡀⠀⠀⠀⠀⠀⠀⢠⣿⣿⠀⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⢀⣴⠟⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⠀⠘⣟⠳⣦⡀⠀⠀⠀⠸⣿⡿⠀⢻⣿⡟⠀⠀⠀⠀⣤⡾⢻⡏⠁⠀⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⠀⠀⢻⡄⢻⠻⣆⠀⠀⠀⠈⠀⠀⠀⠈⠀⠀⠀⢀⡾⢻⠁⢸⠁⠀⠀⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⠀⠀⢸⡇⠀⡆⢹⠒⡦⢤⠤⡤⢤⢤⡤⣤⠤⡔⡿⢁⡇⠀⡿⠀⠀⠀⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⠀⠀⠘⡇⠀⢣⢸⠦⣧⣼⣀⡇⢸⢀⣇⣸⣠⡷⢇⢸⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⠀⠀⠀⣷⠀⠈⠺⣄⣇⢸⠉⡏⢹⠉⡏⢹⢀⣧⠾⠋⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⠀⠀⠀⠻⣆⠀⠀⠀⠈⠉⠙⠓⠚⠚⠋⠉⠁⠀⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀
+    ///⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠶⠦⣤⣤⣤⡤⠶⠞⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    /// </summary>
     public class Parser
     {
         private const string _websiteAdress = "https://schedule.kg-college.ru/";
+        /// <summary>
+        /// Список Ф.И.О. преподавателей, берётся из меню выбора "Преподаватели" на сайте расписания.
+        /// </summary>
         private static List<string> _teacherNames = new List<string>();
+        /// <summary>
+        /// Разделённые Ф.И.О. преподавателей.
+        /// </summary>
         private static List<string> separatedTeacherNames = new List<string>();
         /// <summary>
         /// получить список всех преподавателей, чтобы затем отделять их от текста в названии пары
         /// </summary>
         public static void GetAllTeachers()
         {
-            ChromeDriver driver = new ChromeDriver();
+            ChromeOptions headlessOptions = new ChromeOptions();
+            headlessOptions.AddArguments("headless");
+            ChromeDriver driver = new ChromeDriver(headlessOptions);
             var navigator = driver.Navigate();
             navigator.GoToUrl(_websiteAdress);
             //найти и нажать кнопку Преподаватель
@@ -45,62 +81,64 @@ namespace KGPKScheduleParser
 
         public static ScheduleOfWeek GetScheduleForGroup(string groupName)
         {
-            ChromeDriver driver = new ChromeDriver();
+            ChromeOptions headlessOptions = new ChromeOptions();
+            headlessOptions.AddArguments("headless");
+            ChromeDriver driver = new ChromeDriver(headlessOptions);
             driver.Navigate().GoToUrl(_websiteAdress);
             //найти кнопку "группа" и нажать её
             driver.FindElement(By.XPath("/html/body/div[4]/div/button[1]")).Click();
             //найти селект с списком групп
             SelectElement groups = new SelectElement(driver.FindElement(By.XPath("//*[@id=\"selectgroupname\"]")));
-            //нажать на искомуюю группу, затем выбрать
+            //нажать на искомуюю группу, затем кнопку выбрать
             Thread.Sleep(1000);
             groups.SelectByValue(groupName);
             driver.FindElement(By.XPath("//*[@id=\"fGroupCase\"]/div[2]/button[2]")).Click();
             Thread.Sleep(1000);
             //циклом собираю таблички понедельник, вторник.... в лист
-            List<IWebElement> allWeekdayBoxes = new();
+            List<IWebElement> allDaysOfWeek = new();
             for (int i = 1; i <= 5; i++)
             {
-                allWeekdayBoxes.Add(driver.FindElement(By.XPath($"/html/body/div[4]/div/div[{i}]")));
+                allDaysOfWeek.Add(driver.FindElement(By.XPath($"/html/body/div[4]/div/div[{i}]")));
             }
             //говнокод начинается здесь
             ScheduleOfWeek schedule = new(); //объедок расписания
             //цикл по каждому боксу, который содержит расписание. весь поиск должен происходить в контексте этого бокса
             //я сейчас буду писать ебаную машину тьюринга для работы со строками
-            foreach (var day in allWeekdayBoxes)  //здесь должен создаваться WeekDay
+            foreach (var day in allDaysOfWeek)  //здесь должен создаваться WeekDay
             {
-                var dayTitle = day.FindElement(By.XPath("/html/body/div[4]/div/div[1]/div/div[1]/h5")).Text; //Понедельник / 08.04.2024
-                var dayName = dayTitle.Split('/')[0];
-                var dateOfDay = DateTime.Parse(dayTitle.Split('/')[1]);
-                var thisDayRows = day.FindElements(By.TagName("tr"));
-                DayOfWeek thisDay = new DayOfWeek();
+                var dayTitle = day.FindElement(By.XPath("/html/body/div[4]/div/div[1]/div/div[1]/h5")).Text; //Находим строку Понедельник / 08.04.2024
+                var dayName = dayTitle.Split('/')[0]; //Отделяем название дня недели
+                var dateOfDay = DateTime.Parse(dayTitle.Split('/')[1]); //Отделяем дату
+                var thisDayRows = day.FindElements(By.TagName("tr")); //Получаем все строки данного дня
+                DayOfWeek thisDay = new DayOfWeek(); //объедок дня недели
                 foreach (var tableRow in thisDayRows) //здесь должна создаваться Para
                 {
-                    Thread.Sleep(500);
-                    var ClassNumber = tableRow.FindElement(By.TagName("th")); //номер пары лежит в th
-                    var shitbag = tableRow.FindElements(By.TagName("td")); //номера кабинетов и общая инфа о паре раскиданы в разных td
-                    var comboOfStrings = shitbag[0].Text + " " + shitbag[1].Text; //Сочетание поля с названием пары, преподами итд с полем, в котором написаны кабинеты
+                    Thread.Sleep(500); //ждёмс
+                    var shitbag = tableRow.FindElements(By.TagName("td")); //номера кабинетов и общая инфа о паре раскиданы в разных td. Комбинируем их в одну строку
+                    var ClassNumber = tableRow.FindElement(By.TagName("th")); //номер пары лежит в th. получаем его отдельно.
+                    var comboOfStrings = shitbag[0].Text + " " + shitbag[1].Text; //Создаём сочетание поля с названием пары, преподами итд с полем, в котором написаны кабинеты
                     //надо хотя бы получить кабинеты. По-моему, безопасно взять последние шесть символов из этого сочетания, отпилить их и высосать все числа оттуда
-                    if (!String.IsNullOrWhiteSpace(comboOfStrings)) //ЭТО СЛУЧАЕТСЯ, КОГДА ПАРЫ ВООБЩЕ НЕ СУЩЕСТВУЕТ. ОСТАВЛЯЕМ ВСЁ ПУСТЫМ
+                    if (!String.IsNullOrWhiteSpace(comboOfStrings)) //ЭТО СЛУЧАЕТСЯ, КОГДА ПАРЫ ВООБЩЕ НЕ СУЩЕСТВУЕТ. НАПРИМЕР, ВОСЬМАЯ ПАРА??. ОСТАВЛЯЕМ ВСЁ ПУСТЫМ
                     {
 
-                        var stringWithClassroomNumbers = comboOfStrings.Substring(comboOfStrings.Length - 6); //берём последние 6 символов строки
-                        var ClassroomNumbersTotal = stringWithClassroomNumbers.Where(char.IsDigit); //получаем из строки только числовые символы. превращаем их в числа, соединяем в строку, затем разбиваем на массив
-                        string firstClassNumber = "";
-                        string secondClassNumber = "";
+                        var stringWithClassroomNumbers = comboOfStrings.Substring(comboOfStrings.Length - 6); //берём последние 6 символов общей строки и сохраняем.
+                        var ClassroomNumbersTotal = stringWithClassroomNumbers.Where(char.IsDigit); //получаем из строки только числовые символы. превращаем их в числа, соединяем в строку, затем разбиваем на массив. ЕСЛИ ТАМ c/з, то кабинет будет равен 0
+                        string firstClassNumber = ""; //первый кабинет
+                        string secondClassNumber = ""; //второй кабинет
                         int[] RoomNumbers = new int[2];
 
-                        if (ClassroomNumbersTotal.Count() == 4)
+                        if (ClassroomNumbersTotal.Count() == 4) //если мы в сумме имеем 4 символа в номерах кабинетов, то пары две, разделяем их пополам.
                         {
                             RoomNumbers[0] = Convert.ToInt32(String.Concat(ClassroomNumbersTotal).Substring(0, 2));
                             RoomNumbers[1] = Convert.ToInt32(String.Concat(ClassroomNumbersTotal).Substring(2, 2));
                         }
-                        else
+                        else //значит пара одна
                         {
                             try
                             {
-                                RoomNumbers[0] = Convert.ToInt32(String.Concat(ClassroomNumbersTotal));
+                                RoomNumbers[0] = Convert.ToInt32(String.Concat(ClassroomNumbersTotal)); // пытаемся получить номер кабинета
                             }
-                            catch (Exception)
+                            catch (Exception) //исключение выплёвывается, если не получилось спарсить ни одного числа. ставим кабинет = 0
                             {
                                 RoomNumbers[0] = 0;
                             }
@@ -112,13 +150,24 @@ namespace KGPKScheduleParser
                         Thread.Sleep(500);
 
 
+                        ///
+                        /// Здесь случилась самая страшная проблема
+                        /// Иногда, когда пара разбита по подруппам, преподаватели пар идут в алфавитном порядке, а иногда нет.
+                        /// В том виде, в котором мы получаем всех преподавателей, они всегда в алфавитном порядке
+                        /// То есть, если в расписании стоит Ин.Яз., ведут его
+                        /// Крюкова 1п.
+                        /// Карпенко 2п.
+                        /// А мы в поле имеем Карпенко 1п., Крюкова 2п.
+                        /// Ниже идёт код, который избавляется от этой проблемы
+                        ///
+
 
                         var TeachersForClass = _teacherNames.FindAll(x => comboOfStrings.ToUpperInvariant().Trim().Contains(x.ToUpperInvariant().Trim())).ToList(); // имеет ли в себе _teacherNames такие элементы x, что строка comboOfStrings в себе их содержит
-                        var comboOfStringSplit = comboOfStrings.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)[1].Split();
-                        string SurnameToFind = comboOfStringSplit[0].ToUpperInvariant();
-                        //var firstTeacherSurname = comboOfStringSplit.FirstOrDefault(x => separatedTeacherNames.Contains(x)); //https://www.youtube.com/watch?v=Nppn3Hnocas
-                        var firstTeacherSurname = separatedTeacherNames.First(x => x == SurnameToFind);
-                        if (!TeachersForClass[0].Split(' ')[0].Equals(firstTeacherSurname))
+                        var comboOfStringSplit = comboOfStrings.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)[1].Split(); //разделяем общую строку по лайнбрейку
+                        string SurnameToFind = comboOfStringSplit[0].ToUpperInvariant(); //Фамилия, которую надо найти
+                        //https://www.youtube.com/watch?v=Nppn3Hnocas
+                        var firstTeacherSurname = separatedTeacherNames.First(x => x == SurnameToFind); //получаем фамилию первого реального преподавателя
+                        if (!TeachersForClass[0].Split(' ')[0].Equals(firstTeacherSurname)) //если первая реальная фамилия не сочетается с той, что мы получили в начале, меняем их местами
                         {
                             TeachersForClass.Reverse();
                         }
@@ -145,29 +194,58 @@ namespace KGPKScheduleParser
                         };
                         thisDay.spisokPar.Add(thisClass);
                         thisDay.date = DateOnly.FromDateTime(dateOfDay);
-                        schedule.days.Add(thisDay);
                     }
                 }
+                schedule.days.Add(thisDay);
             }
             driver.Quit();
             return schedule;
         }
     }
-
+    /// <summary>
+    /// Базовое представление пары
+    /// </summary>
     public record Para()
     {
+        /// <summary>
+        /// Порядковый номер пары.
+        /// </summary>
         public int classNumber { get; set; }
+        /// <summary>
+        /// Название всех пар. Пара может быть одна или две.
+        /// </summary>
         public string[] NameOfClasses { get; set; }
+        /// <summary>
+        /// Ф.И.О. Преподавателей. Может быть одно или два
+        /// </summary>
         public string[] TeacherNames { get; set; }
+        /// <summary>
+        /// Номера кабинетов. Может быть один или два
+        /// </summary>
         public int[] ClassroomNumbers { get; set; }
     }
+    /// <summary>
+    /// Базовое представление дня недели
+    /// </summary>
     public record DayOfWeek()
     {
+        /// <summary>
+        /// Пары, которые содержатся в этом дне.
+        /// </summary>
         public List<Para> spisokPar { get; set; } = new List<Para>(7);
+        /// <summary>
+        /// Дата. (мне очень хочется написать день дня)
+        /// </summary>
         public DateOnly date { get; set; }
     }
+    /// <summary>
+    /// Само расписание
+    /// </summary>
     public record ScheduleOfWeek()
     {
+        /// <summary>
+        /// Дни недели с понедельника по пятницу
+        /// </summary>
         public List<DayOfWeek> days { get; set; } = new List<DayOfWeek>(5);
     }
     static class Helper
@@ -217,3 +295,5 @@ namespace KGPKScheduleParser
     }
 
 }
+
+
--- Program.cs
+++ Program.cs
@@ -5,9 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSingleton<Parser>();
+builder.Services.AddHttpClient();
 builder.Services.AddScheduler();
 builder.Services.AddEasyCaching(o =>
 {
@@ -27,22 +26,27 @@ builder.Services.AddEasyCaching(o =>
 
 });
 var app = builder.Build();
+app.UseHttpsRedirection();
 Parser.GetAllTeachers();
 List<string> cachedKeys = new();
 
+//»сполнение кода по определЄнному графику с помощью Coravel, у них хороша€ документаци€.
 app.Services.UseScheduler(s =>
 {
+    //¬ шесть часов утра каждый понедельник мы на вс€кий случай обновл€ем список преподавателей
     s.Schedule(() => Parser.GetAllTeachers())
     .DailyAtHour(6).Monday();
+    //¬ дев€ть часов утра каждое воскресенье мы очищаем все записи из кеша. 
+    //–асчЄт на то, что к этому времени уже должно быть новое раписание, и люди (пользователи(студенты)) будут интересоватьс€ своим расписанием на завтра.
     s.Schedule(async () =>
         {
             var prov = app.Services.GetService<IEasyCachingProvider>();
             await prov.RemoveAllAsync(cachedKeys);
         }).DailyAtHour(9).Sunday();
 });
-app.MapGet("schedule", async ([FromQuery(Name = "group")] string groupname, IEasyCachingProvider prov) =>
+app.MapGet("schedule", async ([FromQuery(Name = "forGroup")] string groupname, IEasyCachingProvider prov) =>
 {
-    if (await prov.ExistsAsync(groupname))
+    if (await prov.ExistsAsync(groupname)) //если в кеше существует запись дл€ искомой группы, вернуть еЄ, иначе спарсить
     {
         var cachedRes = await prov.GetAsync<ScheduleOfWeek>(groupname);
         return Results.Json(cachedRes, options: new System.Text.Json.JsonSerializerOptions()
@@ -52,7 +56,7 @@ app.MapGet("schedule", async ([FromQuery(Name = "group")] string groupname, IEas
         }, statusCode: 200);
     }
     var res = Parser.GetScheduleForGroup(groupname);
-    await prov.SetAsync<ScheduleOfWeek>(groupname, res, TimeSpan.FromDays(1));
+    await prov.SetAsync<ScheduleOfWeek>(groupname, res, TimeSpan.FromDays(6)); //перед возвратом расписани€ оно кешируетс€
     cachedKeys.Add(groupname);
     return Results.Json(res, options: new System.Text.Json.JsonSerializerOptions()
     {
@@ -61,5 +65,16 @@ app.MapGet("schedule", async ([FromQuery(Name = "group")] string groupname, IEas
     }, statusCode: 200);
 
 });
+app.MapGet("/", () =>
+{
+    Parser.GetAllTeachers();
+});
+///
+///  ороче, тема така€. Ётот запрос при первом исполнении занимает ~34-35 секунд. Ёто слишком много
+/// ѕервый запрос дл€ каждой группы все ещЄ будет занимать столько времени, но потом результаты кешируютс€.
+/// —рок годности кеша ради прикола поставил 6 дней, надо будет как-нибудь подумать о реальной жизни и помен€ть это значение.
+/// –асчЄт на то, что расписание - объект неподвижный, не мен€етс€ посреди недели.
+///  огда инфа берЄтс€ из кеша, выполнение запроса занимает меньше одной секунды. ѕо-моему это очень круто.
+///
 app.Run();
 
